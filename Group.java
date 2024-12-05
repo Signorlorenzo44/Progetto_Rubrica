e5984455ee27bc18cfba875973a82589a49b72f6
andreadm1 <a.dimeo11@studenti.unisa.it>
2024-12-05 17:54:24 +0100

    Group.java

--- Group.java
+++ Group.java
@@ -1,14 +1,39 @@
+package com.example.addressbook;
+
 import java.util.List;
 
+/**
+ * La classe rappresenta un gruppo all'interno della rubrica.
+ *
+ * Un gruppo è composto da un nome e da una lista di contatti
+ * associati. È possibile aggiungere o rimuovere contatti da un gruppo.
+ */
 public class Group {
+
+    /**
+     Il nome del gruppo.
+     */
     private String groupName;
+
+    /**
+      La lista dei contatti che appartengono al gruppo.
+     */
     private List<Contact> members;
+
+    /**
+     Aggiunge un contatto al gruppo.
+     * @param contact Il contatto da aggiungere al gruppo.
+     */
     public void addContact(Contact contact) {
         members.add(contact);
     }
+
+    /**
+      Rimuove un contatto dal gruppo.
+     * @param contact Il contatto da rimuovere dal gruppo.
+     */
     public void removeContact(Contact contact) {
         members.remove(contact);
     }
 
 }
-
